a8bb8eb6ff4e57addf4902a95043ff6fdfd74bb9
Jonathan Yau <jonathankyau@gmail.com>
2022-05-15 16:08:00 -0700

    Review Notes

---
+++ superreviewnotes.js
@@ -0,0 +1,57 @@
+// HTML
+
+// What is a program?
+// a set of instructions that you write to tell a computer what to do
+
+// What is programming?
+// Programming is the task of writing those instructions in a language that the computer can understand
+
+// JS
+
+// JS Syntax?
+// Semicolon 
+// Brackets
+// Parenthesis
+// Quotation Marks
+
+// What are variables?
+// We can tell our program to remember values for us to use later on
+// The entity we use to store the value is called a variables
+
+// Declataration: let age
+// Assignment: age = 25
+
+// let age = 25
+
+// Variable Conventions
+// camelCase:
+// let numberOfApples = 9
+
+// Can store strings ("string")
+// Numbers (int: 29 and floats: 5.123)
+// Signed Numbers (int: +4)
+
+// What are Functions?
+// Functions are a simple set of instructions
+// Functions are resusable
+// Functions perform on action as a best practice
+// They form the basic "building blocks" of a program
+
+// function name(parameters) {
+//     //body
+// }
+
+// //call
+// name(arguments)
+
+
+
+// Pseudo Code
+
+//What are loops?
+// repeat an action some number of times
+// three main types of loops in javascript
+// for, while, and do while loops
+// each type offers a different way to determine the start and end points of a loop
+
+// What are Arrays?
