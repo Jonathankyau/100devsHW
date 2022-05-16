b53e97a3436bbf7a50c5a1903940c18dba3e1634
Jonathan Yau <jonathankyau@gmail.com>
2022-05-15 19:10:07 -0700

    super review commit

--- superreviewnotes.js
+++ superreviewnotes.js
@@ -55,3 +55,59 @@
 // each type offers a different way to determine the start and end points of a loop
 
 // What are Arrays?
+// data structure to store ordered collections
+// array elements are numbers starting with zero
+// array have many methods to manage the order of elements
+// can be created by a constructor or literal notation
+
+// Array iteration
+
+// let bestColors = ['green', 'blue', 'red']
+// bestColors.forEach((x,i)=> console.log(x))
+
+// iterates through an array passing in the value and index of each element so first will be green into 'x' and 0 into 'i', then blue and 1, and red and 3
+
+// What are objects?
+
+// objects are collection of variables and functions
+// they represent the attributes and behavior of something used in a PerformanceNavigationTimingobject variables are called properties and object functions are called methods
+// objects store 'keyed' collections
+
+// Factory
+// a prototype is another object that is used as a fallback source of properties
+
+//What are APIs?
+
+// simple interface for complex action
+// think of a restaurant menu
+// lets one thing communicate with another thing without having to know how things are implemented
+
+// OOP
+// Encapsulation
+// the process of storing functions (methods) with their associated data(properties) - in one thing (object)
+
+//ABstraction hides how everything works that the user doesn't need to know to keep things simple
+
+//Inheritance helps us eliminate redundant code. inherit properties from a car making class
+
+//polymorphism is a piece of code that automagically works with a bunch of different objects and makes your code more reusable
+
+//The HUNT
+
+// green square on github
+// hitlist - network and get recommended
+// use the sheet for hitlist - fill out the companies/open positions
+// find folks at those companies that i can talk to
+// 60 networked recommended applications (recruiter, engineering manager, etc)
+// follow them on twitter, find email with hunter.io, ask for coffee chat, send a thank you, get recommendation
+// 10 premium apps with custom projects, cover letters, stories
+// 100hours project
+// interview process- apply/recruited/recommended
+// phone screen -> behavioral -> technical, take home, white board, in-person -> wine and dine interview -> offer
+// once recommended is to know every single step in the process...glassdoor, github, blind....look for every detail of company and how they do interview. ask interviewer what type of interview, what kind of challenge
+// don't go into interview like an accident
+// how to prepare? 
+// The BANK,
+// Behavioral Questions - C.A.R. IS CAUSE, ACTION, RESULT. TALK MY TALK AND HOW I'M VALUABLE, BE POSITIVE, DON'T BE HUMBLE, WHY AM I BETTER BECUASE OF IT
+// P.R.E.P - PARAMETERS, RETURNS, EXAMPLES, PSEUDOCODE...FOR EVERY CODEWARS SPEND 10 MINUTES ON PREP
+// 
